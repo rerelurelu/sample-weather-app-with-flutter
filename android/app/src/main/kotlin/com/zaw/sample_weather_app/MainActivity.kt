bcfd65465e7a1fa755b447a61ac8f6aa53c04b01
zoniha <awaawapurin@gmail.com>
2021-09-01 14:29:30 +0900

    🎉 init: Initial commit

---
+++ android/app/src/main/kotlin/com/zaw/sample_weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zaw.sample_weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:sample_weather_app/views/home_view.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeView(),
+    );
+  }
+}
